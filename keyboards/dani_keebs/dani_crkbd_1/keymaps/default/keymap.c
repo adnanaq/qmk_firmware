5341a49efd9a2fa52fe11acf080a4c30f81cf282
Adnan <dreamboy.dani@gmail.com>
2024-08-29 17:49:21 -0400

    Add RGB keys

--- keyboards/dani_keebs/dani_crkbd_1/keymaps/default/keymap.c
+++ keyboards/dani_keebs/dani_crkbd_1/keymaps/default/keymap.c
@@ -4,7 +4,8 @@
 #include QMK_KEYBOARD_H
 
 enum layers_nameKc {
-  _BASE
+  _BASE,
+  _SECONDARY
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -26,8 +27,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
-    )
+                                            KC_LGUI, MO(_SECONDARY), KC_SPC,     KC_SPC,  KC_ENT,  KC_RALT
+  ),
+
+     [_SECONDARY] = LAYOUT_split_3x6_3(
+        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                            KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+  )
 };
 
 #ifdef OLED_ENABLE
